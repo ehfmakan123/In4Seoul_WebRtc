48ce6e8910e205ffa1989cdcbeb82ffb6f344e69
Hyunkyu Kim <hyunkyu001@gmail.com>
2022-02-04 12:10:11 +0900

    Design: Add Bootstrap-Icons, Add reset.css, Change App.vue 스타일
    bootstrap icon을 사용하기 위해 index.html에 cdn추가
    reset.css파일을 src/common 위치에 추가
    App.vue에서 text-align:center만 남기고 주석처리

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,14 +4,14 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+  /* -webkit-font-smoothing: antialiased; */
+  /* -moz-osx-font-smoothing: grayscale; */
   text-align: center;
-  color: #2c3e50;
+  /* color: #2c3e50; */
 }
 
-#nav {
+/* #nav {
   padding: 30px;
 }
 
@@ -22,5 +22,5 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
