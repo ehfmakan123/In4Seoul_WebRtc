91b3e8f5c8171088e8c1dc8646a4fd694da75c28
Hyunkyu Kim <hyunkyu001@gmail.com>
2022-02-04 18:27:33 +0900

    Design: Add common.css Fix App.vue
    common.css 추가하고 App.vue 모두 주석처리

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,5 +5,6 @@ import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 import '../src/common/css/reset.css'
+import '../src/common/css/common.css'
 
 createApp(App).use(store).use(router).mount('#app')
