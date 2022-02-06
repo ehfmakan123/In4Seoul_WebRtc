1372352ea44780cf31204d6b7f85246d113276a9
limyt0 <80087763+limyt0@users.noreply.github.com>
2022-02-06 20:44:27 +0900

    Design: Feat adminpage
    adminpage design 수정
    
    #158

--- frontend/src/components/admin/AdminStaff.vue
+++ frontend/src/components/admin/AdminStaff.vue
@@ -1,18 +1,17 @@
 <template>
-  <div>
-    <navi-bar/>
-    <router-view/>
-    (staff 관리로 들어왔다!)
-    <br><br>
-
-    <div>
-    <b-container class="bv-example-row mt-3">
+  <div class="row flex-nowrap bg-gray-1">
+     <AdminSidebar />
+    <router-view/>   
+    <div class="w-80 p-5">
+       <h2 class="text-start mt-3 fw-bold">Admin 관리자 페이지</h2>
+    <b-container class="bv-example-row mt-3 ">
 
     <br>
-    <b-row>
-      <b-col>
-        <table hover responsive>
-          <thead head-variant="dark">
+    <div class="bg-white shadow">
+      <div class="text-start p-3 fw-bold bd-bt">스태프 관리</div>
+   
+        <table hover responsive class="table-class  p-3 mb-5 bg-body rounded">
+          <thead head-variant="dark" class="th-class">
             <tr>
               <th>No.</th>
               <th>UserID</th>
@@ -27,12 +26,29 @@
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td colspan="10">
-                <button variant="outline-primary" @click="staffEdit()">
-                 (staff 정보가 여기 삽입됨)
-                </button>
-              </td>
+            <tr class="tr-class" data-bs-placement="top" data-bs-toggle="tooltip" @click="staffEdit()">
+              <td>1</td>
+              <td>hongdong</td>
+              <td>홍길동</td>
+              <td>h2@naver.com</td>
+              <td>010-1234-1234</td>
+              <td>01</td>
+              <td>강남구</td>
+              <td class="table-yes">Y</td>
+              <td>2022-01-01 11:20</td>
+              <td>2022-01-01 11:20</td>
+            </tr>
+            <tr class="tr-class" data-bs-placement="top" data-bs-toggle="tooltip" @click="staffEdit()">
+              <td>1</td>
+              <td>hongdong</td>
+              <td>홍길동</td>
+              <td>h2@naver.com</td>
+              <td>010-1234-1234</td>
+              <td>01</td>
+              <td>강남구</td>
+              <td class="t-red-2">N</td>
+              <td>2022-01-01 11:20</td>
+              <td>2022-01-01 11:20</td>
             </tr>
             <desk-list-row
               v-for="(article, index) in articles"
@@ -41,25 +57,44 @@
             />
           </tbody>
         </table>
-      </b-col>
-    </b-row>
     
+
+    
+    </div>
+
+    <nav aria-label="..." class="d-flex justify-content-center">
+  <ul class="pagination d-flex justify-content-between">
+    <li class="page-item disabled">
+      <a class="page-link" href="#" tabindex="-1" aria-disabled="true">＜</a>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item active" aria-current="page">
+      <a class="page-link" href="#">2</a>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#">＞</a>
+    </li>
+  </ul>
+</nav>
   </b-container>
     </div>
 
 
-
   </div>
+  
 </template>
 
 <script>
-import NaviBar from '@/views/AdminHome.vue'
+
+
+import AdminSidebar from '@/views/AdminHome.vue'
 // @ is an alias to /src
 
 export default {
   name: 'AdminStaff',
   components: {
-    NaviBar,
+    AdminSidebar,
   },
   methods: {
     staffEdit(){
