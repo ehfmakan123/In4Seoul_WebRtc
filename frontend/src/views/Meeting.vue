810d10b4e4174b1c8cbe929d1fcd94119ac1afd5
YeEun <codpdms@naver.com>
2022-02-09 10:50:17 +0900

    Feat: meeting 페이지 안내문 한 줄 추가

--- frontend/src/views/Meeting.vue
+++ frontend/src/views/Meeting.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <h1>화상상담 페이지는 개발중입니다 :)</h1>
   </div>
 </template>
 
@@ -12,3 +13,11 @@ export default {
   }
 }
 </script>
+<style scoped>
+    h1 {
+      font-size: 3rem;
+      font-weight: 600;
+      margin-top: 2rem;
+      margin-left: 2rem;
+    }
+</style>
