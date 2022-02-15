00c5837124e573ab5f3b165cc68ddfbe2c0e5077
limyt0 <80087763+limyt0@users.noreply.github.com>
2022-02-16 02:23:39 +0900

    frontend/pagination: admin and staff pagination 구현
    admin 페이지와 staff pagination 구현
    
    #340 #341 #291

--- frontend/src/views/admin/AdminPost.vue
+++ frontend/src/views/admin/AdminPost.vue
@@ -4,7 +4,7 @@
     <router-view/>
     <div class="w-80 p-5">
        <h2 class="text-start mt-3 fw-bold">Admin 관리자 페이지</h2>
-    <b-container class="bv-example-row mt-3 ">
+    <div class="bv-example-row mt-3 ">
 
     <br>
     <div class="bg-white shadow">
@@ -30,47 +30,50 @@
               />
           </tbody>
         </table>
-         <!-- <tr class="tr-class" data-bs-placement="top" data-bs-toggle="tooltip" @click="postEdit()">
-              <td>1</td>
-              <td>마포구</td>
-              <td>홍대데스크</td>
-              <td>남산같이 가요</td>
-              <td>동행 구합니다!</td>
-              <td>*****</td>
-              <td>2022-01-01 11:20</td>
-              <td>2022-01-01 11:20</td>
-            </tr>
-            <tr class="tr-class" data-bs-placement="top" data-bs-toggle="tooltip" @click="postEdit()">
-              <td>1</td>
-              <td>마포구</td>
-              <td>홍대데스크</td>
-              <td>뮤지컬 프랑켄슈타인</td>
-              <td>블루스퀘어에서 하고있어요...</td>
-              <td>*****</td>
-              <td>2022-01-01 11:20</td>
-              <td>2022-01-01 11:20</td>
-            </tr> -->
-
-    
-    
     </div>
 
     <nav aria-label="..." class="d-flex justify-content-center">
-  <ul class="pagination d-flex justify-content-between">
-    <li class="page-item disabled">
-      <a class="page-link" href="#" tabindex="-1" aria-disabled="true">＜</a>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item active" aria-current="page">
-      <a class="page-link" href="#">2</a>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item">
-      <a class="page-link" href="#">＞</a>
-    </li>
-  </ul>
-</nav>
-  </b-container>
+      <ul class="pagination d-flex justify-content-between">
+        <li v-if="start" class="page-item">
+          <a class="page-link" href="#" @click="gotostartpage">«</a>
+        </li>
+        <li v-else class="page-item disabled">
+          <a class="page-link" href="#" tabindex="-1" aria-disabled="true">«</a>
+        </li>
+
+        <li v-if="pre" li class="page-item">
+          <a class="page-link" href="#" @click="gotoprepage">‹</a>
+        </li>
+        <li v-else li class="page-item disabled">
+          <a class="page-link" href="#" tabindex="-1" aria-disabled="true">‹</a>
+        </li>
+
+        <li
+         v-for="pageitem in pageNumbers"
+          v-bind:id="'p'+pageitem"
+          v-bind:class="{' active': pageitem == nowPage}"
+          :key="pageitem"
+         class="page-item"><a class="page-link" href="#" @click="thispage(pageitem)">{{pageitem}}</a></li>
+        
+        
+        <li v-if="next" class="page-item">
+          <a class="page-link" href="#" @click="gotonextpage">›</a>
+        </li>
+        <li v-else li class="page-item disabled">
+          <a class="page-link" href="#" tabindex="-1" aria-disabled="true">›</a>
+        </li>
+
+        <li v-if="end" class="page-item">
+          <a class="page-link" href="#" @click="gotoendpage">»</a>
+        </li>
+        <li v-else li class="page-item disabled">
+          <a class="page-link" href="#" tabindex="-1" aria-disabled="true">»</a>
+        </li>
+      </ul>
+    </nav>    
+
+
+  </div>
     </div>
   </div>
 </template>
@@ -91,6 +94,16 @@ export default {
     data () {
     return {
       articles: [],
+      pageNumbers: [],
+      totalPage: undefined,
+      totalCount: undefined,
+      nowPage: undefined,
+      startPage: undefined,
+      endPage: undefined,
+      pre: false,
+      next: true,
+      start: false,
+      end: true,    
     }
   },
   props: {
@@ -104,7 +117,7 @@ export default {
   setup(){
 
   },
-    created () {
+  created () {
  
     const token = localStorage.getItem('token')
       const config = {
@@ -114,20 +127,74 @@ export default {
     .then(response => {
       console.log(response.data);
       this.articles = response.data.data;
-      this.pageArray = response.data.data.contacts;
-      // console.log("articles: ")
-      // console.log(this.articles)
-      // console.log("articles.data: ")
-      // console.log(this.articles.data)
+      this.totalPage = response.data.totalPage;
+      this.totalCount = response.data.totalCount;
+      this.nowPage = response.data.nowPage;
+      this.startPage = response.data.startPage;
+      this.endPage = response.data.endPage;
+      this.pre = response.data.pre;
+      this.next = response.data.next;
+      this.start = response.data.start;
+      this.end = response.data.end;
+      this.pageNumbers = []
+      for(var i =this.startPage; i<=this.endPage;i++){
+        this.pageNumbers[i-this.startPage] = i
+      }
     })
     .catch(err => {
       console.log(err);
     });
   },
   methods: {
-    // postEdit(){
-    //   this.$router.push({ name: 'AdminPostEdit' })
-    // },
+    thispage(np){
+      this.nowPage= np
+      const token = localStorage.getItem('token')
+      const config = {
+        Authorization: `Bearer ${token}`
+      }
+      axios.get(`${SERVER_HOST}/admin/board/posts?page=`+this.nowPage, {headers: config})
+      .then(response => {
+        console.log(response.data);
+        this.articles = response.data.data;
+        this.totalPage = response.data.totalPage;
+        this.totalCount = response.data.totalCount;
+        this.nowPage = response.data.nowPage;
+        this.startPage = response.data.startPage;
+        this.endPage = response.data.endPage;
+        this.pre = response.data.pre;
+        this.next = response.data.next;
+        this.start = response.data.start;
+        this.end = response.data.end;
+        this.pageNumbers = []
+        for(var i =this.startPage; i<=this.endPage;i++){
+          this.pageNumbers[i-this.startPage] = i
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    },
+    gotoprepage(){
+      if(this.nowPage-5 < 1 ){
+        this.thispage(1)
+      }else{
+        this.thispage(this.nowPage-5)
+      }
+      
+    },
+    gotonextpage(){
+      if(this.nowPage+5 > this.totalPage){
+        this.thispage(this.totalPage)
+      }else{
+        this.thispage(this.nowPage+5)
+      }
+    },
+    gotostartpage(){
+      this.thispage(1)
+    },
+    gotoendpage(){
+      this.thispage(this.totalPage)
+    }
   }
   
 }
