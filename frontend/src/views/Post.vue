62ea66675b8ce3b81c339a21eef44862f7c88eaf
Hyunkyu Kim <hyunkyu001@gmail.com>
2022-02-16 17:16:21 +0900

    Fix: Change 게시판페이지 높이

--- frontend/src/views/Post.vue
+++ frontend/src/views/Post.vue
@@ -14,7 +14,7 @@
 
     <!-- 오른쪽 구역 -->
     <div class="min-vh-100 w-100">
-      <div class="d-flex justify-content-between">
+      <div class="d-flex justify-content-between" style="height: 11%">
         <div id="go-deskhome" class="p-3">
           <p @click="moveToDeskHome" class=" t-gray-3 text-small">돌아가기</p>
           <p @click="moveToDeskHome" class="arrow-button t-gray-3 text-center mt-2">
